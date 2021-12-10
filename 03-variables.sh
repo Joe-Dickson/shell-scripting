0ad9df583e0fa7fec841ffb4e345cd1b4468bad5
Joe D <ony.dickson@gmail.com>
2021-12-10 10:13:07 -0600

    create vars and access them

--- 03-variables.sh
+++ 03-variables.sh
@@ -17,4 +17,18 @@ c=true    #Boolen
 echo a = $a
 echo a in currency = ${a}USD
 
+## Use cases
+# 1. If we want to use a value in multiple places, then go with variables. This brings advantage of changing the value in one place and it impacts in other places
+## All the above things should be quoted , either single quotes or double quotes, but preferably double quotes
 
+DATE=2021-12-08
+echo Good morning, Welcome, Today date is ${DATE}
+
+## 2. Usecase: If we need to get the data dynamically, we use variable to store that data and we refer
+ # i. Command substitution - VAR=$(command)
+ # ii. Arithmetic substitution VAR=$((expression))
+
+DATE=$(date +%F)
+echo Good morning, Welcome, Today is ${DATE}
+ADD=$((2+3))
+echo ADD = ${ADD}
