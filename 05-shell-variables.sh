8139a8622587a369ea9d7be08a1c93ea0b55a0f2
Joe D <ony.dickson@gmail.com>
2021-12-10 22:31:43 -0600

    Added special variable script

--- 05-shell-variables.sh
+++ 05-shell-variables.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# $0 is nothing but the script name
 echo ${0}
+
+# $1 is the first argument
 echo ${1}
+
+# $2 is the second argument
 echo ${2}
 
+# $* & #@ to get all the input
 echo ${*}
 echo ${@}
 
+# $# is the number of inputs being parsed
 echo ${#}
 
