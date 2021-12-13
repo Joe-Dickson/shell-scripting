0b7e240695f8c9d5f1ad0c09227ef8b986b07825
Joe D <ony.dickson@gmail.com>
2021-12-13 12:09:09 -0600

    Frontend Script Update

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,7 +3,7 @@
 # yum install nginx -y
 # systemctl enable nginx
 # systemctl start nginx
-# Let's download the HTML content that serves the RoboSHop Project UI and deploy under the Nginx path.
+# Let's download the HTML content that serves the roboSHop Project UI and deploy under the Nginx path.
 
 # curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 # Deploy in Nginx Default Location.
@@ -26,7 +26,7 @@
 echo Frontend Setup
 yum install nginx -y
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html ||
 rm -rf *
 nzip /tmp/frontend.zip
 mv frontend-main/* .
