ca3f4387de5154eab7b064bed020cea2e5befd44
Joe D <ony.dickson@gmail.com>
2021-12-14 11:03:33 -0600

    Functions in roboshop

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -33,7 +33,7 @@ yum install nginx -y
 STAT_CHECK $? "Nginx Installation Failed"
 
 
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zi"
 STAT_CHECK $? "Download frontend failed"
 
 cd /usr/share/nginx/html
