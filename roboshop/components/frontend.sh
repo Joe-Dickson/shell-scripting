d416bd5e893ed656e1d1d61f5da28e1efa1b784b
Joe D <ony.dickson@gmail.com>
2021-12-14 11:13:51 -0600

    Functions in roboshop

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -24,8 +24,10 @@
 
 STAT_CHECK() {
   if [ $1 -ne 0 ]; then
-  echo -e "\e[1;31m${2}\e[0m"
+  echo -e "\e[1;31m${2} - FAILED\e[0m"
   exit 1
+  else
+    echo -e "\e1;32m${2} - SUCCESS\e[0m"
   fi
 }
 
@@ -34,7 +36,7 @@ STAT_CHECK $? "Nginx Installation Failed"
 
 
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zi"
-STAT_CHECK $? "Download frontend failed"
+STAT_CHECK $? "Download frontend"
 
 cd /usr/share/nginx/html
 rm -rf *
