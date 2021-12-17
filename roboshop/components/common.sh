26c3f73664ee82622d04a3e842e93fa3ad874739
Joe D <ony.dickson@gmail.com>
2021-12-16 23:53:47 -0600

    Update MongoDB

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -15,7 +15,8 @@ set-hostname -skip-apply ${COMPONENT}
 DOWNLOAD() {
   curl -s -L -o /tmp/${1}.zip "https://github.com/roboshop-devops-project/${1}/archive/main.zip" &>>${LOG_FILE}
   STAT_CHECK $? "Download ${1} code"
-  cd /DumpStack.log.tmpunzip -o /tmp/${1}.zip &>>${LOG_FILE}
+  cd /tmp
+  unzip -o /tmp/${1}.zip &>>${LOG_FILE}
   STAT_CHECK $? "Extract ${1} code"
 }
 
